8ac95d25b3751440646103a7fdc25a98aa32337c
tajemniktv <12156943+tajemniktv@users.noreply.github.com>
2026-07-06 09:34:38 +0000

    fix(website): resolve static route collision for custom 404 page

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
       title: "Docs",
       description: "TajsOS Documentation",
 
+      disable404Route: true,
       components: {
         MarkdownContent: "./src/components/MarkdownContent.astro",
       },
